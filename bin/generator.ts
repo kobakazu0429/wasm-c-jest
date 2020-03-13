b36ebc6acc26eead76c64dca6a2aeb13effc2f44
kobakakzu0429 <kobakazu10@gmail.com>
2020-03-13 21:10:14 +0900

    add bin/generator.ts

---
+++ bin/generator.ts
@@ -0,0 +1,77 @@
+import * as fs from "fs";
+import * as path from "path";
+import MagicString from "magic-string";
+
+const filename = process.argv[2];
+
+if (!filename) {
+  throw new Error(
+    "Error: input file path is required\n$ yarn start <input file path>"
+  );
+}
+
+const targetFilePath = path.join(__dirname, path.relative(__dirname, filename));
+const outputFilePath = path.join(__dirname, "../.tmp/generated.c");
+
+const mainFuncExternPattern = /int main\s*\((.*|\s*)\)/g;
+
+const insertToTopContents = `#include <sys/uio.h>
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+`;
+
+const insertToBottomContents = `
+extern void putc_js(char ch);
+
+WASM_EXPORT
+size_t writev_c(int fd, const struct iovec *iov, int iovcnt)
+{
+  size_t cnt = 0;
+  for (int i = 0; i < iovcnt; i++)
+  {
+    for (int j = 0; j < iov[i].iov_len; j++)
+    {
+      putc_js(((char *)iov[i].iov_base)[j]);
+    }
+    cnt += iov[i].iov_len;
+  }
+  return cnt;
+}
+
+extern char getc_js(int index);
+
+WASM_EXPORT
+size_t readv_c(int fd, const struct iovec *iov, int iovcnt)
+{
+  size_t cnt = 0;
+  for (int i = 0; i < iovcnt; i++)
+  {
+    for (int j = 0; j < iov[i].iov_len; j++)
+    {
+      ((char *)iov[i].iov_base)[j] = getc_js(j);
+    }
+    cnt += iov[i].iov_len;
+  }
+  return cnt;
+}
+`;
+
+fs.readFile(targetFilePath, (err, result) => {
+  if (err) throw err;
+
+  const source = result.toString();
+  const magicString = new MagicString(result.toString());
+
+  let match = mainFuncExternPattern.exec(source);
+  while (match) {
+    magicString.appendLeft(match.index, "WASM_EXPORT\n");
+    match = mainFuncExternPattern.exec(source);
+  }
+
+  magicString.prepend(insertToTopContents);
+  magicString.append(insertToBottomContents);
+
+  const transpiled = magicString.toString();
+
+  fs.writeFileSync(outputFilePath, transpiled);
+});
